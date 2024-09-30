77dcbb3b23e21c611ead2b7780dd297e04b2952b
Nthabiblossom <128476347+Nthabiblossom@users.noreply.github.com>
2024-09-30 20:45:01 +0200

    updated the context for the ai

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "69fo350cf347a61tc6a94bf3497a464e";
   let prompt = `Generate a French poem about ${instructionsInput.value}`;
   let context =
-    "User instructions: You are a dark comedy expert and love to write short poems. Your mission is to generate a four line French poem with basic HTML and separate the line with a <br />. Do not add a title to the poem. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI'";
+    "User instructions: You are a jokes expert and love to write short poems. Your mission is to generate a 4 line French poem in basic HTML and separate each line with a <br />. Do not add a title to the poem. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI'";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
